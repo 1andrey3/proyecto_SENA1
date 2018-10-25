845efc6989910387725155dfa77b90c74fac9225
1andrey3 <caguiza8@misena.edu.co>
2018-10-25 17:02:03 -0500

    falta el ANTIHACKERS

--- application/views/formulario/Formu.php
+++ application/views/formulario/Formu.php
@@ -21,7 +21,7 @@
 					<div id="error" class="col-12 text-center mt-4"></div>
 					
 					<div class="form-group mt-4">
-						<input class="form-control col-6 offset-3" type="Text" name="nodocu" id="doc" placeholder="Documento" maxlength="11" onkeydown="return numeros(event)">
+						<input class="form-control col-6 offset-3" type="Text" name="nodocu" id="doc" placeholder="Documento" maxlength="10" onkeydown="return numeros(event)">
 					</div>
 					<div class="row form-group mt-4">
 						<input class="form-control col-5 offset-1  border border-info" type="text" name="nombres"   id="Nom1" placeholder="Primer Nombre">
@@ -32,20 +32,29 @@
 					</div>
 					<div class="form-row ml-1 mt-4">
 						<input class="form-control  offset-1 col-6" type="text	" name="correo" id="Mail" placeholder="Correo electronico">
-						<label class="input-group-text arroba" for="Mail2">@</label>
+						<label class="input-group-text arroba" for="Mail2">@ </label>
 						<input type="text" class="form-control col-3" placeholder="ej.'Gmail.com'" id="Mail2">
 					</div>
 					<div class="form-group mt-4">
-						<select class="form-control" name="rol" id="rol">
+						<select class="form-control col-5 ml-auto mr-auto" name="rol" id="rol">
 							<option disabled selected value="">Seleccione Rol</option>
 							<option value="Ingeniero">Ingeniero</option>
 							<option value="Contador">Contador</option>
 							<option value="Recepcionista">Recepcionista</option>
 						</select>
 					</div>
-					<!-- <button class="Envio btn btn-light bg-dark text-light"> agregar</button> -->
+							<div class="form-group mt-4">
+								<input type="text" id="telef" class=" col-4 ml-auto mr-auto form-control" placeholder="Teléfono (fijo o móvil)"onkeydown="return numeros(event)" maxlength="10">
+							</div>
+					<div class="row mt-5 ml-auto mr-auto ">
+							<label for="Hombre"class="btn btn-dark  col-3 float-left text-left">Hombre</label>
+							<input class="col-2" type="radio" name="sexo" id="Hombre" value="Hombre">
+							<div class="col-2 text-center bg-light border border-dark rounded">Género</div>
+							<input class="col-2" type="radio" name="sexo" id="Mujer" value="Mujer">
+							<label for="Mujer" class="btn btn-dark   col-3 float-right  text-right">Mujer</label>
+					</div>
 					<div class="form-group">
-						<input class="btn btn-light  bg-dark text-light mt-4 mb-4 col-2 p-3 offset-5" type="submit" name="agregar" id="agregar" >
+						<input class="btn btn-dark bg-dark mt-4 mb-4 col-2 p-3 offset-5" type="submit" name="agregar" id="agregar" >
 					</div>
 					<div class="transiciones">
 					</div>
--- assets/js/validar.js
+++ assets/js/validar.js
@@ -17,9 +17,10 @@ function numeros(e)
 }
 var cont=true;
 var f= true;
-
+var genero=false;
 var validar = function()
 {
+    
     var doc = document.getElementById("doc").value;
     var Nom1 = document.getElementById("Nom1").value;
     var Nom2 = document.getElementById("Nom2").value;
@@ -29,21 +30,34 @@ var validar = function()
     var mail2= document.getElementById("Mail2").value;
     var index= mail2.indexOf(".");
     var mail= Mail.length;
+    var tel = document.getElementById("telef").value;
+    var tell = tel.length;
     console.log(". ="+index);
     console.log("lenght:"+mail)
-    if(doc.length == 0  || Nom1.length==0 ||Nom2.length==0 || App.length==0 || Mail.length==0|| rol.value=="" || mail2.length==0)
+    var MAN= document.getElementById("Hombre").checked;
+    var WOMAN= document.getElementById("Mujer").checked;
+    var aa = document.getElementsByName("sexo").length;
+    console.log("index___"+aa)
+    for(var a=0; a<=aa;a++){
+        if(MAN==true || WOMAN==true){
+            genero=true;
+        }
+    }
+    console.log("men:"+MAN)
+    console.log("woman:"+WOMAN)
+    if(doc.length== 0  || Nom1.length==0 ||Nom2.length==0 || App.length==0 || Mail.length==0|| rol.value=="" || mail2.length==0 || genero==false)
     {
         if(cont==true)
         {
             // $("#error").show('900');
-            document.getElementById("error").innerHTML='<div id="error"><b>Por favor, verifique si tiene campos vacíos.</b></div>';
-            document.getElementById("error").style.border="3px inset black";
+            document.getElementById("error").innerHTML='<div id="error"><b style="font-size:20px;">Por favor, verifique si tiene campos vacíos.</b></div>';
+            document.getElementById("error").style.border="5px inset gray";
             document.getElementById("error").style.transition="0.5s";
             cont=false;
         }else
         {
-            document.getElementById("error").innerHTML = '<div id="error"><b>Por favor, verifique si tiene campos vacíos.</b></div>';
-            document.getElementById("error").style.border="8px inset red";
+            document.getElementById("error").innerHTML = '<div id="error"><b style="font-size:20px;">Por favor, verifique si tiene campos vacíos.</b></div>';
+            document.getElementById("error").style.border="8px outset red";
             cont=true;
         }
         return false;
@@ -52,21 +66,15 @@ var validar = function()
     {
         if (f==true) 
         {
-            var nose = document.getElementById("Mail");
-            nose.addEventListener('blur', function(){
-                if(Mail == "1@a.algo"){
-                    alert("AAAA")
-                }else{
-                    alert("EEEEE")
-                }
-            });
-            
-            document.getElementById("error").innerHTML = '<div class="borde1" id="error"><b>El número de documento no puede tener menos de cinco digitos</b></div>';
+            document.getElementById("error").innerHTML = '<div class="borde1" id="error"><b style="font-size:20px;">El número de documento no puede tener menos de cinco digitos</b></div>';
+            document.getElementById("error").style.border="5px inset gray";
+            document.getElementById("error").style.transition="0.5s";
             document.getElementById("doc").focus();
             f=false;
         }else
         {
-            document.getElementById("error").innerHTML = '<div class="borde" id="error"><b>El número de documento no puede tener menos de cinco digitos</b></div>';
+            document.getElementById("error").innerHTML = '<div class="borde" id="error"><b style="font-size:20px;">El número de documento no puede tener menos de cinco digitos</b></div>';
+            document.getElementById("error").style.border="8px outset red";
             document.getElementById("doc").focus();
             f=true;
         }
@@ -75,7 +83,9 @@ var validar = function()
     if (index != "-1") 
     {
         if(mail2.indexOf('.') == 0){
-            document.getElementById("error").innerHTML = '<div class="borde" id="error"><b>el "." debe tener algo antes y despues de él</b></div>';
+            document.getElementById("error").innerHTML = '<div class="borde1" id="error"><b style="font-size:20px;">Debe haber algo antes del "."</b></div>';
+            document.getElementById("error").style.border="5px inset gray";
+            document.getElementById("error").style.transition="0.5s";
             document.getElementById("Mail2").focus();
             return false;
         }else{
@@ -86,9 +96,26 @@ var validar = function()
         }
     }else
     {
-        document.getElementById("error").innerHTML = '<div class="borde" id="error"><b>Por favor, verifique su email, ya que debe tener un punto después del "@"</b></div>';
+        document.getElementById("error").innerHTML = '<div class="borde" id="error"><b style="font-size:20px;">Por favor, verifique su email, ya que debe tener un punto después del "@"</b></div>';
         document.getElementById("Mail2").focus();
         console.log("NO HAY UN PUNTO")
+        return false;
+    }   
+    if (tell==10 || tell ==7  ) {
+        switch(tell){
+            case 10:{
+                alert("su número es móvil");
+                return false;
+            }
+            case 7:{
+                var fijo = '031 '+tel;
+                document.getElementById("telef").innerHTML='<input type="text" id="telef" value="'+fijo+'">';
+                alert("su número"+tel+"se le añadirá el 031, quedando \n"+fijo);
+                return false;
+            }
+        }
+    }else{
+        alert("no es ni 10 ni 7")
         return false;
     }
 }
